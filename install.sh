b3d9686615a288ecb51cba6c4ebc7105bb44fe63
Coleman <cole@imakethe.com>
2020-10-06 13:27:52 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -17,20 +17,45 @@ defaults write -g com.apple.swipescrolldirection -bool FALSE
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew analytics off
 
-# Quality of life stuff
-brew install iterm2
-# TODO: Install iTerm Profile
 
-# Install Coding Tools
+##### Install Coding Tools
 brew cask install jetbrains-toolbox
 brew cask install visual-studio-code
 brew install cocoapods
 brew install fastlane
 
+# Dart && Flutter Version Manager
 brew tap dart-lang/dart
 brew install dart
 pub global activate fvm
 echo 'export PATH="$PATH":"$HOME/.pub-cache/bin"' >> ~/.zshrc
 
+# Python Version Manager
+brew install pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.zshrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.zshrc
 
-# Misc Installs
+
+# Node Version Manager
+brew install nvm
+mkdir ~/.nvm || true;
+echo 'export NVM_DIR="$HOME/.nvm"' >> ~/.zshrc
+echo '  [ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm' >> ~/.zshrc
+echo '  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && . "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion' >> ~/.zshrc
+
+##### Common and Misc Installs
+brew install iterm2
+# TODO: Install iTerm Profile
+brew cask install sizeup
+brew cask install cleanmymac
+brew cask install mailplane
+brew cask install synergy
+brew cask install 1password
+brew cask install 1password-cli
+
+# This is part of pyenv and has to be put at the end of the profile
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zshrc
+
+# Starship makes ther terminal look cool
+brew install starship
+echo 'eval "$(starship init zsh)"' >> ~/.zshrc # this has to be very very last
